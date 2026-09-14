afd8d85547f62fa39e0826a1eb05572eadad2dcf
chananyanara <chananya.nara@bumail.net>
2026-09-14 19:38:25 +0700

    FINISH HOMEWORK

--- Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Mage.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Mage.cs
@@ -1,9 +1,21 @@
+using Mono.Cecil;
 using UnityEngine;
 
 namespace Assignment.StudentSolution
 {
-    public class Mage
+    public class Mage : RangeEnemy
     {
+        public int mana;
+
+        public override void Attack(Entity target)
+        {
+            
+        }
+
+        public void CastSpell(Entity target)
+        {
+            
+        }
 
     }
 }
--- Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Player.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Player.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 namespace Assignment.StudentSolution
 {
-    public class Player
+    public class Player : Entity
     {
+        public int score;
+        private Item[] items;
+
+        public void CollectItem(Item item)
+        {
+            
+        }
+
+        protected void LevelUp()
+        {
+            
+        }
 
     }
 }
